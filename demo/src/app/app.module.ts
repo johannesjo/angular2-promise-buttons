c292fa0c70c4676170045b59861acdea5bd8c9ea
Johannes Millan <johannes.millan@gmail.com>
2017-05-04 23:35:30 +0200

    chore: cleanup modules

--- demo/src/app/app.module.ts
+++ demo/src/app/app.module.ts
@@ -1,7 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
@@ -11,8 +9,6 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
