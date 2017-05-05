0b16e3fe2b48eadf2d8e54f276311543d64e3fd6
Johannes Millan <johannes.millan@gmail.com>
2017-05-05 17:34:43 +0200

    0.1.4

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+<a name="0.1.4"></a>
+## [0.1.4](https://github.com/johannesjo/angular2-promise-buttons/compare/v0.1.3...v0.1.4) (2017-05-05)
+
+
+
 <a name="0.1.3"></a>
 ## [0.1.3](https://github.com/johannesjo/angular2-promise-buttons/compare/v0.1.2...v0.1.3) (2017-05-05)
 
--- dist/angular2-promise-button.component.js
+++ dist/angular2-promise-button.component.js
@@ -1,40 +1,29 @@
-System.register(["@angular/core"], function (exports_1, context_1) {
-    "use strict";
-    var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-        var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-        if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-        else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-        return c > 3 && r && Object.defineProperty(target, key, r), r;
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var core_1 = require("@angular/core");
+var Angular2PromiseButtonComponent = (function () {
+    function Angular2PromiseButtonComponent() {
+    }
+    Angular2PromiseButtonComponent.prototype.ngOnInit = function () {
     };
-    var __metadata = (this && this.__metadata) || function (k, v) {
-        if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-    };
-    var __moduleName = context_1 && context_1.id;
-    var core_1, Angular2PromiseButtonComponent;
-    return {
-        setters: [
-            function (core_1_1) {
-                core_1 = core_1_1;
-            }
-        ],
-        execute: function () {
-            Angular2PromiseButtonComponent = (function () {
-                function Angular2PromiseButtonComponent() {
-                }
-                Angular2PromiseButtonComponent.prototype.ngOnInit = function () {
-                };
-                return Angular2PromiseButtonComponent;
-            }());
-            Angular2PromiseButtonComponent = __decorate([
-                core_1.Component({
-                    selector: 'app-angular2-promise-button',
-                    templateUrl: './angular2-promise-button.component.html',
-                    styleUrls: ['./angular2-promise-button.component.css']
-                }),
-                __metadata("design:paramtypes", [])
-            ], Angular2PromiseButtonComponent);
-            exports_1("Angular2PromiseButtonComponent", Angular2PromiseButtonComponent);
-        }
-    };
-});
+    return Angular2PromiseButtonComponent;
+}());
+Angular2PromiseButtonComponent = __decorate([
+    core_1.Component({
+        selector: 'app-angular2-promise-button',
+        templateUrl: './angular2-promise-button.component.html',
+        styleUrls: ['./angular2-promise-button.component.css']
+    }),
+    __metadata("design:paramtypes", [])
+], Angular2PromiseButtonComponent);
+exports.Angular2PromiseButtonComponent = Angular2PromiseButtonComponent;
 //# sourceMappingURL=angular2-promise-button.component.js.map
