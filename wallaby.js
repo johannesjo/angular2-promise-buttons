d9192af94eaec88ec84e6a99936d256bd4c1831f
Johannes Millan <johannes.millan@gmail.com>
2017-05-06 17:31:09 +0200

    try to get wallaby running

---
+++ wallaby.js
@@ -0,0 +1,96 @@
+'use strict';
+
+const wallabyWebpack = require('wallaby-webpack');
+const path = require('path');
+
+const compilerOptions = Object.assign(
+  require('./tsconfig.json').compilerOptions,
+  require('./demo/src/tsconfig.spec.json').compilerOptions);
+
+module.exports = function (wallaby) {
+
+  const webpackPostprocessor = wallabyWebpack({
+    entryPatterns: [
+      'scripts/test.ts',
+      'src/**/*spec.js',
+      'demo/src/**/*spec.js'
+    ],
+
+    module: {
+      loaders: [
+        { test: /\.css$/, loader: 'raw-loader' },
+        { test: /\.html$/, loader: 'raw-loader' },
+        { test: /\.js$/, loader: 'angular2-template-loader', exclude: /node_modules/ },
+        { test: /\.json$/, loader: 'json-loader' },
+        { test: /\.styl$/, loaders: ['raw-loader', 'stylus-loader'] },
+        { test: /\.less$/, loaders: ['raw-loader', 'less-loader'] },
+        { test: /\.scss$|\.sass$/, loaders: ['raw-loader', 'sass-loader'] },
+        { test: /\.(jpg|png)$/, loader: 'url-loader?limit=128000' }
+      ]
+    },
+
+    resolve: {
+      modules: [
+        path.join(wallaby.projectCacheDir, 'demo/src'),
+        path.join(wallaby.projectCacheDir, 'src')
+      ]
+    }
+  });
+
+  return {
+    files: [
+      { pattern: 'node_modules/zone.js/dist/zone.js', instrument: false },
+      { pattern: 'node_modules/zone.js/dist/long-stack-trace-zone.js', instrument: false },
+      { pattern: 'node_modules/zone.js/dist/proxy.js', instrument: false },
+      { pattern: 'node_modules/zone.js/dist/sync-test.js', instrument: false },
+      { pattern: 'node_modules/zone.js/dist/jasmine-patch.js', instrument: false },
+      { pattern: 'node_modules/zone.js/dist/async-test.js', instrument: false },
+      { pattern: 'node_modules/zone.js/dist/fake-async-test.js', instrument: false },
+      { pattern: 'node_modules/reflect-metadata/Reflect.js', instrument: false },
+      { pattern: 'src/**/*.ts', load: false },
+      { pattern: 'src/**/*.d.ts', ignore: true },
+      { pattern: 'src/**/*.css', load: false },
+      { pattern: 'src/**/*.less', load: false },
+      { pattern: 'src/**/*.scss', load: false },
+      { pattern: 'src/**/*.sass', load: false },
+      { pattern: 'src/**/*.styl', load: false },
+      { pattern: 'src/**/*.html', load: false },
+      { pattern: 'src/**/*.json', load: false },
+      { pattern: 'src/**/*spec.ts', ignore: true },
+      { pattern: 'demo/src/**/*.ts', load: false },
+      { pattern: 'demo/src/**/*.d.ts', ignore: true },
+      { pattern: 'demo/src/**/*.css', load: false },
+      { pattern: 'demo/src/**/*.less', load: false },
+      { pattern: 'demo/src/**/*.scss', load: false },
+      { pattern: 'demo/src/**/*.sass', load: false },
+      { pattern: 'demo/src/**/*.styl', load: false },
+      { pattern: 'demo/src/**/*.html', load: false },
+      { pattern: 'demo/src/**/*.json', load: false },
+      { pattern: 'demo/src/**/*spec.ts', ignore: true }
+    ],
+
+    tests: [
+      { pattern: 'src/**/*spec.ts', load: false }
+    ],
+
+    testFramework: 'jasmine',
+    preprocessors: {
+      './scripts/test.ts': ['@angular/cli']
+    },
+    compilers: {
+      '**/*.ts': wallaby.compilers.typeScript(compilerOptions)
+    },
+
+    env: {
+      kind: 'electron'
+    },
+
+    postprocessor: webpackPostprocessor,
+
+    setup: function () {
+      window.__moduleBundler.loadTests();
+    },
+
+    debug: true
+  };
+};
