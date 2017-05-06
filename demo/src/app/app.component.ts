5fe08eb68e9f59316a99e2d2aaa009038b2862f7
Johannes Millan <johannes.millan@gmail.com>
2017-05-07 00:37:54 +0200

    feature: include all basic examples from ng1 version

--- demo/src/app/app.component.ts
+++ demo/src/app/app.component.ts
@@ -1,28 +1,99 @@
 import {Component} from '@angular/core';
 
+const STANDARD_DELAY = 1000;
+const FAKE_FACT = {
+  success: function () {
+    return new Promise((fulfill) => {
+      setTimeout(() => {
+        fulfill({
+          msg: 'SUCCESS'
+        });
+      }, STANDARD_DELAY);
+    });
+  },
+  error: () => {
+    return new Promise((fulfill, reject) => {
+      setTimeout(() => {
+        reject({
+          msg: 'ERROR'
+        });
+      }, STANDARD_DELAY);
+    });
+  },
+  endless: () => {
+    return new Promise((fulfill) => {
+      setTimeout(fulfill, 99999999);
+    });
+  }
+};
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
-  initialPromise: Promise<any>;
-  onClickPromise: Promise<any>;
+  successPromise: Promise<any>;
+  errorPromise: Promise<any>;
+  endlessInitialPromise: Promise<any>;
+  endlessPromise: Promise<any>;
+  submitPromise: Promise<any>;
+  chainedPromises: any;
+  promiseIndex: number;
 
   constructor() {
-    this.initialPromise = new Promise((fulfill, reject) => {
-      setTimeout(fulfill, 99999999);
-    });
+    this.endlessInitial();
+    // this.promiseIndex = 0;
   }
 
-  onClick() {
-    this.onClickPromise = new Promise((fulfill, reject) => {
-      setTimeout(() => {
-        console.log('PROMISE DONE');
-        fulfill();
-      }, 3000);
-    });
-    console.log('PROMISE', this.onClickPromise);
+  success($event: any): Promise<any> {
+    console.log($event);
+    this.successPromise = FAKE_FACT.success();
+    return this.successPromise;
+  }
+
+  successDirectlyReturnedPromise(): Promise<any> {
+    return FAKE_FACT.success();
+  }
+
+  error() {
+    this.errorPromise = FAKE_FACT.error()
+      .catch(() => {
+        console.log('YEAH ERROR');
+      });
+  }
+
+  endless() {
+    this.endlessPromise = FAKE_FACT.endless();
+  }
+
+  endlessInitial() {
+    this.endlessInitialPromise = FAKE_FACT.endless();
+  }
+
+  submit() {
+    this.submitPromise = FAKE_FACT.success();
+  }
+
+  submitDirectlyReturnPromise(): Promise<any> {
+    return FAKE_FACT.success();
+  }
+
+  chain() {
+    this.promiseIndex = 0;
+    this.chainedPromises = this.countChain()
+      .then(this.countChain.bind(this))
+      .then(this.countChain.bind(this))
+      .then(this.countChain.bind(this))
+      .then(this.countChain.bind(this));
+
+    return this.chainedPromises;
+  }
+
+  countChain() {
+    return FAKE_FACT.success()
+      .then(() => {
+        this.promiseIndex++;
+      });
   }
 }
