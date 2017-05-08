c6d1370e45530f4ebec59404883a670ac3bf3f2c
Johannes Millan <johannes.millan@gmail.com>
2017-05-08 17:07:55 +0200

    feature: add unit tests for default directive config

--- src/promise-btn.directive.spec.ts
+++ src/promise-btn.directive.spec.ts
@@ -1,6 +1,11 @@
 import {ElementRef} from '@angular/core';
+import {Component} from '@angular/core';
+import {DebugElement} from '@angular/core';
 import {async, TestBed} from '@angular/core/testing';
+import {ComponentFixture} from '@angular/core/testing';
 import {PromiseBtnDirective} from './promise-btn.directive';
+import {userCfg} from './user-cfg';
+import {By} from '@angular/platform-browser';
 
 class MockElementRef extends ElementRef {
   constructor() {
@@ -9,30 +14,153 @@ class MockElementRef extends ElementRef {
   }
 }
 
-describe('setup', () => {
+@Component({
+  selector: 'test-component',
+  template: ''
+})
+class TestComponent {
+  asyncMethod: any;
+  testPromise: Promise<any>;
+}
 
+describe('PromiseBtnDirective', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
+        TestComponent,
         PromiseBtnDirective
       ],
       providers: [
         // more providers
-        // {provide: ElementRef, useClass: MockElementRef}
+        {
+          provide: ElementRef,
+          useClass: MockElementRef
+        },
+        {
+          provide: userCfg, useClass: class {
+        }
+        },
       ]
-    }).compileComponents();
-
-    TestBed.overrideComponent(PromiseBtnDirective, {
-      set: {
-        template: '<button promiseBtn="testPromise">BUTTON_TEXT</button>'
-      }
     });
   }));
 
-  describe('PromiseBtnDirective', () => {
-    it('should create an instance', () => {
-      const directive = new PromiseBtnDirective();
-      expect(directive).toBeTruthy();
+  let fixture: ComponentFixture<TestComponent>;
+  let buttonDebugElement: DebugElement;
+  let buttonElement: HTMLButtonElement;
+  let promiseBtnDirective: PromiseBtnDirective;
+
+  beforeEach(() => {
+    fixture = TestBed.overrideComponent(TestComponent, {
+      set: {
+        template: '<button (click)="asyncMethod($event)" [promiseBtn]="testPromise">BUTTON_TEXT</button>'
+      }
+    }).createComponent(TestComponent);
+    fixture.detectChanges();
+
+    buttonDebugElement = fixture.debugElement.query(By.css('button'));
+    buttonElement = <HTMLButtonElement> buttonDebugElement.nativeElement;
+    promiseBtnDirective = buttonDebugElement.injector.get<PromiseBtnDirective>(PromiseBtnDirective);
+  });
+
+  describe('default cfg', () => {
+    describe('basic init', () => {
+      it('should create an instance', () => {
+        expect(promiseBtnDirective).toBeDefined();
+        expect(promiseBtnDirective.cfg).toBeDefined();
+        // const directive = new PromiseBtnDirective({}, {});
+        // expect(directive).toBeTruthy();
+      });
+      it('should append the spinner el to the button', () => {
+        const spinnerEl = buttonElement.querySelector('span');
+        expect(spinnerEl.outerHTML).toBe('<span class="btn-spinner"></span>');
+      });
+    });
+
+    describe('once a promise is passed', () => {
+      beforeEach(() => {
+        fixture.componentInstance.testPromise = new Promise(() => {
+        });
+        spyOn(promiseBtnDirective, 'initLoadingState').and.callThrough();
+        fixture.detectChanges();
+      });
+
+      it('should init the loading state', () => {
+        expect(promiseBtnDirective.initLoadingState).toHaveBeenCalled();
+      });
+      it('should add .is-loading class', async(() => {
+        fixture.whenStable().then(() => {
+          expect(buttonElement.className).toBe('is-loading');
+        });
+      }));
+      it('should disable the button', async(() => {
+        fixture.whenStable().then(() => {
+          expect(buttonElement.getAttribute('disabled')).toBe('disabled');
+        });
+      }));
+    });
+
+    describe('once a passed promise is resolved', () => {
+      let promise;
+      let resolve: any;
+      beforeEach(async(() => {
+        promise = new Promise((res) => {
+          resolve = res;
+        });
+        fixture.componentInstance.testPromise = promise;
+
+        // test init before to be sure
+        spyOn(promiseBtnDirective, 'initLoadingState').and.callThrough();
+        fixture.detectChanges();
+        expect(promiseBtnDirective.initLoadingState).toHaveBeenCalled();
+
+        fixture.whenStable().then(() => {
+          spyOn(promiseBtnDirective, 'cancelLoadingStateIfPromiseAndMinDurationDone').and.callThrough();
+          resolve();
+        });
+        fixture.detectChanges();
+      }));
+
+      it('should cancel the loading state', () => {
+        expect(promiseBtnDirective.cancelLoadingStateIfPromiseAndMinDurationDone).toHaveBeenCalled();
+      });
+      it('should remove the .is-loading class', () => {
+        expect(buttonElement.className).toBe('');
+      });
+      it('should enable the button', () => {
+        expect(buttonElement.hasAttribute('disabled')).toBe(false);
+      });
+    });
+
+    describe('once a passed promise is rejected', () => {
+      let promise;
+      let reject: any;
+      beforeEach(async(() => {
+        promise = new Promise((res, rej) => {
+          reject = rej;
+        });
+        fixture.componentInstance.testPromise = promise;
+
+        // test init before to be sure
+        spyOn(promiseBtnDirective, 'initLoadingState').and.callThrough();
+        fixture.detectChanges();
+        expect(promiseBtnDirective.initLoadingState).toHaveBeenCalled();
+
+        fixture.whenStable().then(() => {
+          spyOn(promiseBtnDirective, 'cancelLoadingStateIfPromiseAndMinDurationDone').and.callThrough();
+          reject();
+        });
+        fixture.detectChanges();
+      }));
+
+      it('should cancel the loading state', () => {
+        expect(promiseBtnDirective.cancelLoadingStateIfPromiseAndMinDurationDone).toHaveBeenCalled();
+      });
+      it('should remove the .is-loading class', () => {
+        expect(buttonElement.className).toBe('');
+      });
+      it('should enable the button', () => {
+        expect(buttonElement.hasAttribute('disabled')).toBe(false);
+      });
     });
   });
 });
