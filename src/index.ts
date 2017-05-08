f0ff3457304d786b2989b4fbddbe79e1a5b1f595
Johannes Millan <johannes.millan@gmail.com>
2017-05-08 18:06:04 +0200

    feature: move promise button module into its own file and use index to expose all sub modules

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,2 @@
-import {NgModule} from '@angular/core';
-import {ModuleWithProviders} from '@angular/core';
-import {PromiseBtnDirective} from './promise-btn.directive';
-import {userCfg} from './user-cfg';
-
-@NgModule({
-  declarations: [
-    PromiseBtnDirective,
-  ],
-  imports: [],
-  exports: [
-    PromiseBtnDirective,
-  ],
-  providers: []
-})
-export class Angular2PromiseButtonModule {
-  // add forRoot to make it configurable
-  static forRoot(userCfgPassedByUser?: any): ModuleWithProviders {
-    const cfg = userCfgPassedByUser || {};
-    return {
-      ngModule: Angular2PromiseButtonModule,
-      providers: [{provide: userCfg, useValue: cfg}]
-    };
-  }
-}
+export {PromiseBtnDirective} from './promise-btn.directive'
+export {Angular2PromiseButtonModule} from './angular2-promise-buttons.module';
