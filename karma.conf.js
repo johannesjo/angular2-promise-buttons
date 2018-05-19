0c802c81e719f70afed089ee72c6d26750e15721
Johannes Millan <johannes.millan@gmail.com>
2018-05-19 16:41:11 +0200

    chore: fix unit tests not running

--- karma.conf.js
+++ karma.conf.js
@@ -4,22 +4,19 @@
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    frameworks: [
+      '@angular-devkit/build-angular',
+      'jasmine'
+    ],
     plugins: [
+      require('@angular-devkit/build-angular/plugins/karma'),
       require('karma-jasmine'),
       //require('karma-chrome-launcher'),
       require('karma-phantomjs-launcher'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
-    },
-    files: [
-      { pattern: './scripts/test.ts', watched: false }
-    ],
-    preprocessors: {
-      './scripts/test.ts': ['@angular-devkit/build-angular']
     },
     mime: {
       'text/x-typescript': ['ts', 'tsx']
