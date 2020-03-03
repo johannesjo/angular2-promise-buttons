700458d7acbea887cb0faf4914f8676ea6e39374
Johannes Millan <johannes.millan@gmail.com>
2020-03-03 16:17:24 +0100

    chore: fix test

--- karma.conf.js
+++ karma.conf.js
@@ -24,7 +24,7 @@ module.exports = function (config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: true,
+    autoWatch: false,
     browsers: ['ChromeHeadless'],
     singleRun: true,
     customLaunchers: {
