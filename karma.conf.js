8e5594b407eece012ae303cf998d47544f04e751
Johannes Millan <johannes.millan@gmail.com>
2020-03-03 16:14:04 +0100

    chore: add headless testing

--- karma.conf.js
+++ karma.conf.js
@@ -1,46 +1,56 @@
 // Karma configuration file, see link for more information
-// https://karma-runner.github.io/0.13/config/configuration-file.html
+// https://karma-runner.github.io/1.0/config/configuration-file.html
 
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: [
-      '@angular-devkit/build-angular',
-      'jasmine'
-    ],
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
-      require('@angular-devkit/build-angular/plugins/karma'),
       require('karma-jasmine'),
-      //require('karma-chrome-launcher'),
-      require('karma-phantomjs-launcher'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    mime: {
-      'text/x-typescript': ['ts', 'tsx']
-    },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, 'coverage'), reports: [
-        'lcovonly',
-        'text-summary',
-        'html'
-      ],
-      fixWebpackSourcePaths: true,
+      dir: require('path').join(__dirname, '../coverage'),
+      reports: ['html', 'lcovonly'],
+      fixWebpackSourcePaths: true
     },
-    angularCli: {
-      config: './angular-cli.json',
-      environment: 'dev'
-    },
-    reporters: config.angularCli && config.angularCli.codeCoverage
-      ? ['progress', 'coverage-istanbul']
-      : ['progress'],
+    reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['PhantomJS'],
-    singleRun: true
+    browsers: ['ChromeHeadless'],
+    singleRun: true,
+    customLaunchers: {
+      'ChromeHeadless': {
+        base: 'Chrome',
+        flags: [
+          // We must disable the Chrome sandbox when running Chrome inside Docker
+          // (Chrome's sandbox needs more permissions than Docker allows by default)
+          '--headless',
+          '--no-sandbox',
+          '--disable-gpu',
+          '--no-default-browser-check',
+          '--no-first-run',
+          '--disable-default-apps',
+          '--disable-popup-blocking',
+          '--disable-translate',
+          '--disable-background-timer-throttling',
+          '--disable-renderer-backgrounding',
+          '--disable-device-discovery-notifications',
+          // Without a remote debugging port, Google Chrome exits immediately.
+          '--remote-debugging-port=9222',
+          '--disable-web-security',
+        ],
+        debug: true
+      }
+    },
+    browserNoActivityTimeout: 120000,
   });
 };
